aafb1e678145632f45919d848b50f4facb4610d5
Ritika Rathi <90149136+rritika389@users.noreply.github.com>
2023-07-25 11:28:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import{ fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -21,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
